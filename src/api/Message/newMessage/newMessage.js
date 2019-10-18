358869e43d84fbe487454bba0e3696b66070bfe3
hongjoonki <ghdjk2216@naver.com>
2019-10-18 18:26:15 +0900

    new messages

--- src/api/Message/newMessage/newMessage.js
+++ src/api/Message/newMessage/newMessage.js
@@ -1,9 +1,22 @@
-// import { prisma } from "../../../../generated/prisma-client";
-// import { isAuthenticated } from "../../../middlewares";
-// import { ROOM_FRAGMENT } from "../../../fragments";
+import { prisma } from "../../../../generated/prisma-client";
 
-// export default {
-//     newMessage:()=>{
-        
-//     }
-// }
+export default {
+    Subscription: {
+        newMessage: {
+            subscribe: (_, args) => {
+                const { roomId } = args;
+                return prisma.$subscribe.message({
+                    AND: [
+                        { mutation_in: "CREATED" },
+                        {
+                            node: {
+                                room: { id: roomId }
+                            }
+                        }
+                    ]
+                }).node();
+            },
+            resolve: payload => payload
+        }
+    }
+};
